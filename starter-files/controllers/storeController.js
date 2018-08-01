5c0361c1196126b05fdfacb331ab76875baef0db
David Carter <dajocarter@gmail.com>
2018-08-01 14:13:29 -0700

    Create route in controller and add to router

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -1,3 +1,7 @@
-exports.homePage = (Request, Response) => {
-  Response.render("index");
+exports.homePage = (req, res) => {
+  res.render("index");
+};
+
+exports.addStore = (req, res) => {
+  res.render("editStore", { title: "Add Store" });
 };
