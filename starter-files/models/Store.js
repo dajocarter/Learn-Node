3c2d86e62bdf66b8b6b4fceec9add09af9e5e11b
David Carter <dajocarter@gmail.com>
2018-08-05 03:39:42 -0700

    Make store slugs unique

--- starter-files/models/Store.js
+++ starter-files/models/Store.js
@@ -37,15 +37,18 @@ const storeSchema = new mongoose.Schema({
   photo: String
 });
 
-storeSchema.pre("save", function(next) {
+storeSchema.pre("save", async function(next) {
   if (!this.isModified("name")) {
     next(); // Skip it
     return; // Stop this function from running
   }
   this.slug = slug(this.name);
+  const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, "i");
+  const storesWithSlug = await this.contructor.find({ slug: slugRegEx });
+  if (storesWithSlug.length) {
+    this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+  }
   next();
-
-  // TODO: Make sure slugs are unique
 });
 
 module.exports = mongoose.model("Store", storeSchema);
