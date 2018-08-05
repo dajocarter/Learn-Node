597d21f18e7e8a69f33e4554545d390ab57d1def
David Carter <dajocarter@gmail.com>
2018-08-05 03:25:00 -0700

    Create route for single store

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -73,3 +73,12 @@ exports.updateStore = async (req, res) => {
   );
   res.redirect(`/stores/${store._id}/edit`);
 };
+
+exports.getStoreBySlug = async (req, res) => {
+  const store = await Store.findOne({ slug: req.params.slug });
+  if (!store) {
+    next();
+    return;
+  }
+  res.render("store", { store, title: store.name });
+};
