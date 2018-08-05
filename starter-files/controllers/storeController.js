b29be385a44cb7c27464c95572a6c6b67a4dc41f
David Carter <dajocarter@gmail.com>
2018-08-05 04:07:13 -0700

    Create routes for tags
    archive and single

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -82,3 +82,9 @@ exports.getStoreBySlug = async (req, res) => {
   }
   res.render("store", { store, title: store.name });
 };
+
+exports.getStoresByTag = async (req, res) => {
+  const tags = await Store.getTagsList();
+  const tag = req.params.tag;
+  res.render("tag", { tags, tag, title: "Tags" });
+};
